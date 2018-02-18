62f76c8bc2a0cf57223a9f8d77122c13c4b4011f
Sarah Maris <sarah@sarahmaris.com>
2018-02-17 23:25:10 -0500

    Add title and colors

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Would You Rather????</h1>
+        <header className="app-header">
+          <h1 className="app-title">Would You Rather????</h1>
         </header>
       </div>
     );
