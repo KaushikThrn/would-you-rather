db205bb5b46c6077a3feb62b7fa0b1e21e18bf63
Sarah Maris <sarah@sarahmaris.com>
2018-02-18 00:36:59 -0500

    Add nav buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,41 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
+import {Link, NavLink, Route, Switch} from 'react-router-dom'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="app-header">
+    return (<div className="App">
+      <header className="app-header">
+        <Link className="home-link" to="/">
           <h1 className="app-title">Would You Rather????</h1>
-        </header>
-      </div>
-    );
+        </Link>
+      </header>
+      <nav className="navList">
+        <NavLink
+          to="/"
+          activeClassName="current"
+          className="unNav navButton">
+          Unanswered Questions
+        </NavLink>
+        <NavLink
+          to="/answered"
+          activeClassName="current"
+          className="ansNav navButton">
+          Answered Questions
+        </NavLink>
+        <NavLink
+          to="/leaderboard"
+          activeClassName="current"
+          className="leadNav navButton">
+          Leaderboard
+        </NavLink>
+        <NavLink
+          to="/add"
+          activeClassName="current"
+          className="newNav navButton">
+          Add Question
+        </NavLink>
+      </nav>
+    </div>)
   }
 }
 
