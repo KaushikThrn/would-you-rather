850b41b07cdf8d94ed93ae414c98a3d72e49d53b
Sarah Maris <sarah@sarahmaris.com>
2018-02-20 16:55:59 -0500

    Show questions on page (not sorted)

---
+++ src/actions/index.js
@@ -0,0 +1,29 @@
+import { _getQuestions } from '../utils/_data.js'
+
+export const GET_QUESTIONS = 'GET_QUESTIONS'
+
+/* API request error handling */
+const showError = (error) =>
+  console.log('fetch failed: ' , error.statusText);
+
+
+// load posts
+const getQuestions = (questions) => {
+  return {
+    type: GET_QUESTIONS,
+    questions
+  }
+}
+
+export const loadQuestions = () => {
+  console.log("Load")
+  return dispatch => {
+    _getQuestions()
+      .then((response) => {
+        // convert questions to array
+        const qIndices = Object.keys(response)
+        const questions = qIndices.map(index =>  response[index] )
+        dispatch(getQuestions(questions))
+      })
+  }
+}
